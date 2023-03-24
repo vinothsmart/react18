67227ac92f422c4cda9045c9d2bc59efbcd69668
vinotsmart <s.vinothkanna90@gmail.com>
2023-03-24 15:01:58 +0530

    first comp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const Greeting = () => {
-  return <div>My First Component</div>;
+  return <h2>My First Component</h2>;
 };
 
-export default Greeting;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Greeting />, root);

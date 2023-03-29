73ea7d309fd34f65d3bb2494782022588af36022
vinotsmart <s.vinothkanna90@gmail.com>
2023-03-29 17:27:17 +0530

    created booklist components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Greeting = () => {
+const BookList = () => {
   return (
-    <>
-      <h1 className="someClass">Hello World</h1>
-      <ul>
-        <li>
-          <a href="https://github.com/vinothsmart/">Hi Vinoth</a>
-        </li>
-      </ul>
-      <input type="text" />
-    </>
+    <section>
+      <Book />
+    </section>
   );
 };
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Title = () => <h2>Book Title</h2>;
+const Author = () => <h2>Author</h2>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />, root);
+root.render(<BookList />, root);

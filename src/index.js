acbab0fc9052baf9438481ddd2522dac5500330e
vinotsmart <s.vinothkanna90@gmail.com>
2023-03-29 16:26:55 +0530

    react fragament added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Greeting = () => {
-  return <h2>My First Component</h2>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <ul>
+        <li>
+          <a href="https://github.com/vinothsmart/">Hi Vinoth</a>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

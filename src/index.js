4403b61560dc0aeb027931ad39d0c85e807cb650
vinotsmart <s.vinothkanna90@gmail.com>
2023-03-29 18:02:37 +0530

    css added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const BookList = () => {
   return (
     <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
       <Book />
       <Book />

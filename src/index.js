debc98c0ee2abd74a2e5f394b215fdd8c3a157e7
vinotsmart <s.vinothkanna90@gmail.com>
2023-04-10 16:42:29 +0530

    props

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const author = "Dav Pilkey";
+const title = "Dog Man: Twenty Thousand Fleas Under the Sea";
+const img = "./images/book1.jpg";
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job="Front Developer" />
+      <Book title="React" experience={5} />
     </section>
   );
 };
 
-const author = "Dav Pilkey";
-const Book = () => {
-  const title = "Dog Man: Twenty Thousand Fleas Under the Sea";
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="./images/book1.jpg"
-        alt="Dog Man: Twenty Thousand Fleas Under the Sea"
-      />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.experience}</p>
     </article>
   );
 };

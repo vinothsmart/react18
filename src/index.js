37392b3fc00dbb1ad6ceb890dc1d507b8053fb0f
vinotsmart <s.vinothkanna90@gmail.com>
2023-04-17 11:13:34 +0530

    dynamic values

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const author = "Dav Pilkey";
-const title = "Dog Man: Twenty Thousand Fleas Under the Sea";
-const img = "./images/book1.jpg";
+const firstBook = {
+  author: "Dav Pilkey",
+  title: "Dog Man: Twenty Thousand Fleas Under the Sea",
+  img: "./images/book1.jpg",
+};
+
+const secondBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };

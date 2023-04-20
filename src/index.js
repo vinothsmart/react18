8bc4250483e81741336dd40c172ea34025324cef
vinotsmart <s.vinothkanna90@gmail.com>
2023-04-20 14:51:47 +0530

    children props

--- src/index.js
+++ src/index.js
@@ -37,12 +37,13 @@ const BookList = () => {
   );
 };
 
-const Book = ({ img, title, author }) => {
+const Book = ({ img, title, author, children }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 };

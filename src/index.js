f440f3b7430c3728fbf29aebf15996277f250817
vinotsmart <s.vinothkanna90@gmail.com>
2023-04-25 13:21:54 +0530

    object as prop

--- src/index.js
+++ src/index.js
@@ -21,8 +21,7 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { id, img, title, author } = book;
-        return <Book key={id} img={img} title={title} author={author}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );

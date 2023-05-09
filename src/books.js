026dd72d89db540cf4b022f779e8c1ecedd4f4ca
vinotsmart <s.vinothkanna90@gmail.com>
2023-05-09 15:14:16 +0530

    export const

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: "Dav Pilkey",
+    title: "Dog Man: Twenty Thousand Fleas Under the Sea",
+    img: "./images/book1.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-const books = [
-  {
-    author: "Dav Pilkey",
-    title: "Dog Man: Twenty Thousand Fleas Under the Sea",
-    img: "./images/book1.jpg",
-    id: 1,
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
 
 const BookList = () => {
   return (

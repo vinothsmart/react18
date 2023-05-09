6d2cb476b3f15fb9a0719b6db4d323d83f93bea2
vinotsmart <s.vinothkanna90@gmail.com>
2023-05-09 11:26:39 +0530

    fix1

--- src/index.js
+++ src/index.js
@@ -35,11 +35,16 @@ const BookList = () => {
 const Book = (props) => {
   const { img, title, author, getBook, id } = props;
   // console.log(props);
+
+  const getSingleBook = () => {
+    getBook(id);
+  };
+
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={getBook(id)}>display title</button>
+      <button onClick={getSingleBook}>display title</button>
       <h4>{author}</h4>
     </article>
   );

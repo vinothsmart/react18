71c70a4ff84166b39a4c3170e703fdcd55e63d71
vinotsmart <s.vinothkanna90@gmail.com>
2023-05-29 14:53:25 +0530

    service component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { services } from "../data";
+import Service from "./Service";
 import Title from "./Title";
 
 const Services = () => {
@@ -7,16 +8,8 @@ const Services = () => {
     <section className="section services" id="services">
       <Title title="our" subTitle="services" />
       <div className="section-center services-center">
-        {services.map(({ id, icon, title, text }) => (
-          <article key={id} className="service">
-            <span className="service-icon">
-              <i className={icon}></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">{title}</h4>
-              <p className="service-text">{text}</p>
-            </div>
-          </article>
+        {services.map((serivce) => (
+          <Service key={serivce.id} {...serivce} />
         ))}
       </div>
     </section>

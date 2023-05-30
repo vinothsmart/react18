8c16a84e0ab132db1bab6be5fa110ef554039493
vinotsmart <s.vinothkanna90@gmail.com>
2023-05-30 16:19:50 +0530

    Splitted more into component

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Service = ({ id, icon, title, text }) => {
+const Service = ({ icon, title, text }) => {
   return (
     <article className="service">
       <span className="service-icon">

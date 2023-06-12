35373a3e3500596f4063397e6b82e9e73b7cee4d
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-12 11:17:05 +0530

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import ErrorExample from './tutorial/01-useState/starter/01-error-example';
+import UseStateBasics from './tutorial/01-useState/starter/02-useState-basics';
 
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React</h2>
-      <ErrorExample />
+      <UseStateBasics />
     </div>
   );
 }
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <h2>{count}</h2>
+      <button type='button' onClick={handleClick} className='btn'>
+        Increment
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;

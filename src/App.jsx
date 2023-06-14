2dd691a0376710bfcc367046eaccbb789e152417
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-14 13:38:01 +0530

    useState multiple objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import UseStateArray from './tutorial/01-useState/starter/03-useState-array';
+import UseStateObject from './tutorial/01-useState/starter/04-useState-object';
 
 function App() {
   return (
     <div className='container'>
-      <UseStateArray />
+      <UseStateObject />
     </div>
   );
 }
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,24 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [values, setValues] = useState({
+    name: 'Vinoth',
+    age: 34,
+    hobby: 'programming',
+  });
+  const handleShow = () => {
+    setValues((prevValues) => ({ ...prevValues, name: 'Kanna' }));
+  };
+  return (
+    <>
+      <h3>{values.name}</h3>
+      <h3>{values.age}</h3>
+      <h4>Enjoys : {values.hobby}</h4>
+      <button className='btn' onClick={handleShow}>
+        Show Me
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;

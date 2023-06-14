b4adda1be4b60b00d986e6eee2b9ad4e77e2f439
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-14 10:25:11 +0530

    rm and clear items

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -2,21 +2,27 @@ import { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  var array = [1, 2, 3, 4, 5];
-  var array2 = array;
-
-  array = [];
-
-  console.log(array); // [ ];
-  console.log(array2); // [ 1, 2, 3, 4, 5 ]
   const [pepople, setPeople] = useState(data);
+  const removeItem = (id) => {
+    let newPeople = pepople.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearItems = () => {
+    setPeople([]);
+  };
   return (
     <>
-      {pepople.map((person) => (
-        <div key={person.id}>
-          <h4>{person.name}</h4>
+      {pepople.map(({ id, name }) => (
+        <div key={id}>
+          <h4>{name}</h4>
+          <button className='btn' onClick={() => removeItem(id)}>
+            remove
+          </button>
         </div>
       ))}
+      <button className='btn' style={{ marginTop: '2em' }} onClick={clearItems}>
+        clear items
+      </button>
     </>
   );
 };

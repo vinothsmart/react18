e1139fc15a71b3758ad86e41bc4ca47388e1150d
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-15 17:09:39 +0530

    object using state

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [values, setValues] = useState({
+  const [person, setPerson] = useState({
     name: 'Vinoth',
     age: 34,
     hobby: 'programming',
   });
   const handleShow = () => {
-    setValues((prevValues) => ({ ...prevValues, name: 'Kanna' }));
+    setPerson((prevValues) => ({
+      ...prevValues,
+      name: 'John',
+      age: 25,
+      hobby: 'video games',
+    }));
   };
   return (
     <>
-      <h3>{values.name}</h3>
-      <h3>{values.age}</h3>
-      <h4>Enjoys : {values.hobby}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys : {person.hobby}</h4>
       <button className='btn' onClick={handleShow}>
         Show Me
       </button>

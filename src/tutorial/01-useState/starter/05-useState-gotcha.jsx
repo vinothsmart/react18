2df69fdfe2225212785b02cf3b9e05f411430c97
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-16 13:25:47 +0530

    using previous state and prevState

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useCallback, useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = useCallback(() => {
+    // setValue((prevState) => prevState + 1);
+    setTimeout(() => {
+      setValue((prevState) => prevState + 1);
+    }, 2000);
+  }, []);
+
+  return (
+    <h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={handleClick}>
+        Increase
+      </button>
+    </h2>
+  );
 };
 
 export default UseStateGotcha;

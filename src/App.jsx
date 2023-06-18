15f99c1f5cec774f5d170ad83c36fbfe46e32536
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-18 11:28:21 +0530

    multiple useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import CodeExample from './tutorial/02-useEffect/starter/02-useEffect-basics';
+import MultipleEffects from './tutorial/02-useEffect/starter/03-multiple-effects';
 
 function App() {
   return (
     <div className='container'>
-      <CodeExample />
+      <MultipleEffects />
     </div>
   );
 }

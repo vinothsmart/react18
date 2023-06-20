3c239901e873d4df7d7a33808569de1f523190fd
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-20 16:15:58 +0530

    condi rendering

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  const handleLogin = () => {
+    setUser({ id: 1, username: 'Vino' });
+  };
+  const handleLogout = () => {
+    setUser(null);
+  };
+  return (
+    <div>
+      {user ? (
+        <>
+          <h2>Hello there {user.username}</h2>
+          <button className='btn' onClick={handleLogout}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>Please Login</h2>
+          <button className='btn' onClick={handleLogin}>
+            Login
+          </button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;

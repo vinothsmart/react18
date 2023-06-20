49911d3f60912ab9c9659a7b326947e145749f1b
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-20 15:07:49 +0530

    rendering comp

--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -8,7 +8,31 @@ const ShortCircuitExamples = () => {
   const [user, setUser] = useState({ name: 'john' });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <div>
+      <h2>{text || 'Default Value'}</h2>
+      {text && (
+        <div>
+          <h2>what ever return</h2>
+          <h2>{name}</h2>
+        </div>
+      )}
+      {/* {!text && (
+        <div>
+          <h2>what ever return</h2>
+          <h2>{name}</h2>
+        </div>
+      )} */}
+      {user && <SomeComponent name={user.name} />}
+    </div>
+  );
 };
+
+const SomeComponent = ({ name }) => (
+  <div>
+    <h2>what ever return</h2>
+    <h2>{name}</h2>
+  </div>
+);
 
 export default ShortCircuitExamples;

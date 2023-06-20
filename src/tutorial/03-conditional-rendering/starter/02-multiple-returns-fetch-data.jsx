fa86b7fdc218d39fcd468b5d4ce5880251ba334b
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-20 11:22:10 +0530

    fetch user

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,22 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(url);
+        const user = await response.json();
+        // setUser(user);
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return <h2>Fetch Data </h2>;
 };
 export default MultipleReturnsFetchData;

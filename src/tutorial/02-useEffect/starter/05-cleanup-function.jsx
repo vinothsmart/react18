67136ee3b6e5aa3eab46700e86f502c13054a767
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-20 16:35:21 +0530

    cleanup function

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,35 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  console.log('render');
+  return (
+    <>
+      <button className='btn' onClick={handleClick}>
+        Toggle Component
+      </button>
+      {toggle && <RandomComponent />}
+    </>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log('Component Mounted');
+    const intId = setInterval(() => {
+      console.log('Hello');
+    }, 2000);
+    return () => {
+      console.log('Component Unmounted');
+      clearInterval(intId);
+      console.log('Cleanup Function');
+    };
+  }, []);
+  return <h2>Random Component</h2>;
 };
 
 export default CleanupFunction;

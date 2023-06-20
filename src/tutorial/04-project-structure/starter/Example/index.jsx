77f064d5ff3f14b0b2d4e52028fde2b42c121863
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-20 19:22:54 +0530

    Example

---
+++ src/tutorial/04-project-structure/starter/Example/index.jsx
@@ -0,0 +1,14 @@
+import FirstComponent from './FirstComponent';
+import SecondComponent from './SecondComponent';
+
+const Example = () => {
+  return (
+    <>
+      <h2>Example</h2>
+      <FirstComponent />
+      <SecondComponent />
+    </>
+  );
+};
+
+export default Example;

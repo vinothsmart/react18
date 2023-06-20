2a958eef126fdff621cbdac40520bf702aca9429
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-20 12:20:47 +0530

    loading and display user data

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,22 +2,38 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  const [user, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const response = await fetch(url);
         const user = await response.json();
-        // setUser(user);
-        console.log(user);
+        setUser(user);
       } catch (error) {
+        setIsError(true);
         console.log(error);
       }
+      setIsLoading(false);
     };
     fetchUser();
   }, []);
 
-  return <h2>Fetch Data </h2>;
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>Error...</h2>;
+
+  return (
+    <>
+      <img
+        style={{ width: '150px', borderRadius: '25px' }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2>{user.name}</h2>
+      <p>{user.bio}</p>
+    </>
+  );
 };
 export default MultipleReturnsFetchData;

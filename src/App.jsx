47fb26dc8b94c8dfa5e8e24eaf0e828b5d21f789
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-29 14:08:09 +0530

    userChallange

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import ControlledInputs from './tutorial/06-forms/starter/01-controlled-inputs';
+import UserChallenge from './tutorial/06-forms/starter/02-user-challenge';
 
 function App() {
   return (
     <div className='container'>
-      <ControlledInputs />
+      <UserChallenge />
     </div>
   );
 }
--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,13 +1,35 @@
+import { useState, useCallback } from 'react';
+
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+
+  const handleChange = useCallback((e) => {
+    setName(e.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(name);
+    },
+    [name]
+  );
+
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type='text'
+            className='form-input'
+            id='name'
+            value={name}
+            onChange={handleChange}
+          />
         </div>
 
         <button type='submit' className='btn btn-block'>

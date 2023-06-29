af90a8efdf305ebbbc783588f38a53ae2ffffb23
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-29 13:04:39 +0530

    issue sloved imageloaded

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,7 +1,8 @@
-const Person = ({ id, name, nickName, images }) => {
+const Person = ({ id, name, nickName = '', images }) => {
+  const img = images && images[0].small.url;
   return (
     <div key={id}>
-      <img src={images.small.url} alt={name} style={{ width: '50px' }} />
+      <img src={img} alt={name} style={{ width: '50px' }} />
       <h4>{name}</h4>
       <p>Nickname: {nickName}</p>
     </div>

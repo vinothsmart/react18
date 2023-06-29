1d31a30a9c01291b106608d9a3b14a16b6c73445
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-29 13:59:11 +0530

    controlled comp

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,56 @@
+import { useState, useCallback } from 'react';
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [formData, setFromData] = useState({
+    name: '',
+    email: '',
+  });
+
+  const handleChange = useCallback((e) => {
+    const name = e.target.id;
+    const value = e.target.value;
+    setFromData((prevData) => ({ ...prevData, [name]: value }));
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(formData.name, formData.email);
+    },
+    [formData]
+  );
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <h4>controlled inputs</h4>
+      <div className='form-row'>
+        <label htmlFor='name' className='form-label'>
+          name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          id='name'
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          Email
+        </label>
+        <input
+          type='email'
+          className='form-input'
+          id='email'
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <button type='submit' className='btn btn-block'>
+        submit
+      </button>
+    </form>
+  );
 };
 export default ControlledInputs;

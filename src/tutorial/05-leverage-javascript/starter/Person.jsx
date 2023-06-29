0730d02e7d87ab10802c4f9c8f772504fb1565ab
vinotsmart <s.vinothkanna90@gmail.com>
2023-06-29 13:27:40 +0530

    leverage issue all are fixed

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,5 +1,10 @@
+import avatar from '../../../assets/default-avatar.svg';
+
 const Person = ({ id, name, nickName = '', images }) => {
-  const img = images && images[0].small.url;
+  //   const img = images?.[0]?.small?.url || avatar;
+  //   optional chaining operator (?.) to avoid errors when the data is not available
+  const img = images?.[0]?.small?.url ?? avatar;
+
   return (
     <div key={id}>
       <img src={img} alt={name} style={{ width: '50px' }} />

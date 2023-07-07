f3072d13ed33fe9f934430c611cae2f2f1d32a73
vinotsmart <s.vinothkanna90@gmail.com>
2023-07-07 12:58:00 +0530

    useRef

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -2,10 +2,26 @@ import { useEffect, useRef, useState } from 'react';
 
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
+  const refContainer = useRef(null);
+  // console.log(refContainer);
+  const isMounted = useRef(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const name = refContainer.current.value;
+    console.log(name);
   };
+  useEffect(() => {
+    // console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+    }
+    console.log('re-render');
+  }, [value]);
 
   return (
     <div>
@@ -14,7 +30,12 @@ const UseRefBasics = () => {
           <label htmlFor='name' className='form-label'>
             Name
           </label>
-          <input type='text' id='name' className='form-input' />
+          <input
+            type='text'
+            id='name'
+            className='form-input'
+            ref={refContainer}
+          />
         </div>
         <button type='submit' className='btn btn-block'>
           submit

1604d9fb0a475436186eb4b05b81796b324dbadd
vinotsmart <s.vinothkanna90@gmail.com>
2023-07-07 13:15:32 +0530

    use toogle custome hook

---
+++ src/tutorial/08-custom-hooks/starter/useToggle.js
@@ -0,0 +1,12 @@
+import { useCallback } from 'react';
+import { useState } from 'react';
+
+const useToggle = (defaultValue) => {
+  const [show, setShow] = useState(defaultValue);
+  const toggle = useCallback(() => {
+    setShow(!show);
+  }, [show]);
+  return { show, toggle };
+};
+
+export default useToggle;

c6de7c3816bf0f07a7737d67bcd12a29e8d549a5
vinothsmart <s.vinothkanna90@gmail.com>
2023-07-23 11:14:15 +0530

    useTranstiion optimzation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import LowerState from './tutorial/11-performance/starter/03-hooks';
+import LatestReact from './tutorial/11-performance/starter/04-react-18';
 
 function App() {
   return (
     <div className='container'>
-      <LowerState />
+      <LatestReact />
     </div>
   );
 }

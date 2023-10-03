5ea17f7be77955f5dedd5742ef7dffc386ef2739
vinotsmart <s.vinothkanna90@gmail.com>
2023-10-03 09:45:51 +0530

    data added

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+import data from './data';
 const App = () => {
+  const [peeple, setPeople] = useState(data);
+  console.log(peeple);
   return <h2>Birthday Reminder - Starter</h2>;
 };
 export default App;

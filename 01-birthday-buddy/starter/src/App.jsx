562387662fa62c924a79a290bdc89f85b84aab27
vinotsmart <s.vinothkanna90@gmail.com>
2023-10-10 11:52:30 +0530

    data added

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import data from './data';
 import List from './List';
+
+const usersDataURL = 'https://jsonplaceholder.typicode.com/users';
 const App = () => {
   const [people, setPeople] = useState(data);
+
+  const [list, setList] = useState([]);
+
+  const fetchUsers = useCallback(async () => {
+    const response = await fetch(usersDataURL);
+    const data = await response.json();
+    setList(data);
+  }, []);
+
+  const clearAll = useCallback(() => {
+    setPeople([]);
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
-        <button type="button" onClick={() => setPeople([])} className="btn btn-block">
+        <button type="button" onClick={clearAll} className="btn btn-block">
           Clear All
         </button>
+        <h1> List Users Page</h1>
+        {list.map(({ id, name }) => (
+          <div key={id}>
+            <div className="row">
+              <div className="col-lg-4">
+                <p>{name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </section>
     </main>
   );

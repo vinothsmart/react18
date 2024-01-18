f16f48816174957d0cb21843e3a6a462a90bfa77
vinotsmart <s.vinothkanna90@gmail.com>
2024-01-18 18:19:14 +0530

    rm key child

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const SocialLink = ({ id, href, itemClass, icon }) => {
+const SocialLink = ({ href, itemClass, icon }) => {
   return (
-    <li key={id}>
+    <li>
       <a href={href} target="_blank" className={itemClass} rel="noreferrer">
         <i className={icon}></i>
       </a>

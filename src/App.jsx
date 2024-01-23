b79a9ffa94f83bf8b9e19fd7abee3e1877f717c1
vinotsmart <s.vinothkanna90@gmail.com>
2024-01-23 16:04:59 +0530

    old comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import FetchData from './tutorial/02-useEffect/starter/04-fetch-data';
 import LatestReact from './tutorial/11-performance/starter/04-react-18';
 
 function App() {
   return (
     <div className='container'>
-      {/* <LatestReact /> */}
-      <FetchData />
+      <LatestReact />
     </div>
   );
 }

8ad414fec0d14d5b5ed8b5c89b7d7858f49d53a6
vinotsmart <s.vinothkanna90@gmail.com>
2024-02-12 13:37:06 +0530

    updated with callback

--- src/tutorial/08-custom-hooks/starter/useToggle.js
+++ src/tutorial/08-custom-hooks/starter/useToggle.js
@@ -3,8 +3,8 @@ import { useCallback, useState } from 'react';
 const useToggle = (defaultValue) => {
   const [show, setShow] = useState(defaultValue);
   const toggle = useCallback(() => {
-    setShow(!show);
-  }, [show]);
+    setShow((prev) => !prev);
+  }, []);
   return { show, toggle };
 };
 

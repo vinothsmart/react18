c0f3f812332710f89138d6338e965e48bab2f51a
vinothsmart <s.vinothkanna90@gmail.com>
2024-02-12 12:05:18 +0530

    Update form component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-// import LatestReact from './tutorial/11-performance/starter/04-react-18';
-
-import UserChallenge from './tutorial/06-forms/starter/02-user-challenge';
+import UncontrolledInputs from './tutorial/06-forms/starter/05-form-data';
 
 function App() {
   return (
     <div className='container'>
-      <UserChallenge />
+      <UncontrolledInputs />
     </div>
   );
 }

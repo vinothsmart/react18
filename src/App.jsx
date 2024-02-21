08d1171cc639e3d48287046e6630702955fd07d3
vinotsmart <s.vinothkanna90@gmail.com>
2024-02-21 15:30:03 +0530

    Update import statement in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import LowerStateChallenge from './tutorial/11-performance/starter/02-lower-state-challenge';
+import LowerState from './tutorial/11-performance/starter/03-hooks';
 
 function App() {
   return (
     <div className='container'>
-      <LowerStateChallenge />
+      <LowerState />
     </div>
   );
 }

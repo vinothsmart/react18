a978af8b1bed495dac07cbea9221a531b2a39996
vinotsmart <s.vinothkanna90@gmail.com>
2024-02-27 18:57:51 +0530

    Add useState hook and FaBeer icon

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import { FaBeer } from "react-icons/fa";
+import people from "./data";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <>
+      <h2>Reviews Starter</h2>
+      <FaBeer className="beer" />
+    </>
+  );
 };
 export default App;

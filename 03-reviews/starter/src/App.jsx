ff562d4e8c26e3788290b107549b7ddb11ac54dc
vinotsmart <s.vinothkanna90@gmail.com>
2024-02-29 11:50:54 +0530

    Refactor App component to display a single review

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -7,9 +7,19 @@ const App = () => {
   const { name, job, image, text } = people[index];
 
   return (
-    <>
-      <h2>Reviews Starter</h2>
-    </>
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="authour">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
   );
 };
 export default App;

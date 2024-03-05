5e4d95bfe3fbcadcab425e982fe54739e1bbbee4
vinotsmart <s.vinothkanna90@gmail.com>
2024-03-05 18:20:17 +0530

    Add useState hook and menu data

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Title from "./Title";
+import menu from "./data";
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState([menu]);
   return (
     <main>
       <section className="menu">

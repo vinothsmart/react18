f42136f7bef20528e0f9a52a9de1738e5164700f
vinotsmart <s.vinothkanna90@gmail.com>
2024-03-06 18:40:35 +0530

    Add loading state and fetch jobs data in App component

--- 06-tabs/starter/src/App.jsx
+++ 06-tabs/starter/src/App.jsx
@@ -1,6 +1,32 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = useCallback(async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchJobs();
+  }, [fetchJobs]);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;

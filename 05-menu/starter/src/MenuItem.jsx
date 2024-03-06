6e39b1df7e8fabacb7efd94fa67abb5398615dab
vinotsmart <s.vinothkanna90@gmail.com>
2024-03-06 11:51:29 +0530

    Refactor MenuItem component to include image, title, price, and description

--- 05-menu/starter/src/MenuItem.jsx
+++ 05-menu/starter/src/MenuItem.jsx
@@ -1,5 +1,16 @@
-const MenuItem = ({ id }) => {
-  return <h4>MenuItem</h4>;
+const MenuItem = ({ img, title, price, desc }) => {
+  return (
+    <article className="menu">
+      <img src={img} alt={title} className="img" />
+      <div className="item-info">
+        <header>
+          <h5>{title}</h5>
+          <span className="item-price">${price}</span>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
+    </article>
+  );
 };
 
 export default MenuItem;

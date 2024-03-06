f43acd3dbd316d0fc4d4e33eb3566a0f1b43d6d1
vinotsmart <s.vinothkanna90@gmail.com>
2024-03-06 14:07:34 +0530

    Add categories state to App component

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -3,8 +3,12 @@ import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
 
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
   return (
     <main>
       <section className="menu">

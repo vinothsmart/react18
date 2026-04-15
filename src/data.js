b9b612ed208a038230a3de0734ea10cdea4de73f
vinothsmart <s.vinothkanna90@gmail.com>
2026-04-15 11:41:43 +0530

    update socialLinks to correct URLs for Twitter and Squarespace

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const pageLinks = [
 export const socialLinks = [
   {
     id: 1,
-    href: "https://www.twitter.com",
+    href: "https://www.facebook.com",
     icon: "fab fa-facebook",
   },
   {
@@ -23,7 +23,7 @@ export const socialLinks = [
   },
   {
     id: 3,
-    href: "https://www.twitter.com",
+    href: "https://www.squarespace.com",
     icon: "fab fa-squarespace",
   },
 ];
